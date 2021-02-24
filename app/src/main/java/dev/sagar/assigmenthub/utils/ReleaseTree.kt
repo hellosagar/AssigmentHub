1cc7c85a67dc144ef032962544b0839faa41080f
hellosagar <sagarkhurana00786@gmail.com>
2021-02-25 04:04:03 +0530

    Test Build Again!

--- app/src/main/java/dev/sagar/assigmenthub/utils/ReleaseTree.kt
+++ app/src/main/java/dev/sagar/assigmenthub/utils/ReleaseTree.kt
@@ -7,7 +7,7 @@ import timber.log.Timber
 class ReleaseTree : @NotNull Timber.Tree() {
     override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
         if (priority == Log.ERROR || priority == Log.WARN) {
-            // SEND ERROR REPORTS TO YOUR Crashlytics.
+            // Send Error.
         }
     }
 }
